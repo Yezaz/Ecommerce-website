40b11ce9823148cdd166d9a4187395ea9dccc330
Yezaz <114501702+Yezaz@users.noreply.github.com>
2024-07-02 21:48:49 +0530

    Update LoginSignup.jsx

--- frontend/src/pages/LoginSignup.jsx
+++ frontend/src/pages/LoginSignup.jsx
@@ -20,7 +20,7 @@ export default function LoginSignup() {
   // Functions for login and signup which link to the backend
   const login = async () => {
     let responseData;
-    await fetch('http://localhost:4000/login', {
+    await fetch('https://ecommerce-website-backend-ecru.vercel.app//login', {
       method: 'POST',
       headers: {
         Accept: 'application/form-data',
@@ -42,7 +42,7 @@ export default function LoginSignup() {
 
   const signup = async () => {
     let responseData;
-    await fetch('http://localhost:4000/signup', {
+    await fetch('https://ecommerce-website-backend-ecru.vercel.app//signup', {
       method: 'POST',
       headers: {
         Accept: 'application/form-data',
